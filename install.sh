b5fe8357a9563f85dcc9e5eb00304b204672a2f8
DevinHe <huopo125@gmail.com>
2014-06-21 09:30:07 +0800

    add install env infos and gitignore files

---
+++ install.sh
@@ -0,0 +1,13 @@
+# install ssh
+sudo apt-get install ssh
+# install p7zip-full
+sudo apt-get install p7zip-full
+sudo apt-get install ssh
+# screenshot
+sudo apt-get ksnapshot
+# install pdf editor
+sudo apt-get install xournal
+# install scribes text editor
+sudo add-apt-repository ppa:mystilleef/scribes-daily
+sudo apt-get update
+sudo apt-get install scribes
